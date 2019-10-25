f2b2499922813c96c7c0de6882eebac5a1bc486e
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-10-25 01:11:50 -0400

    connected to db

---
+++ config/db.js
@@ -0,0 +1,27 @@
+
+//this file contains the mongoDB connection, using mongoose to connect
+const mongoose = require('mongoose');
+
+//bring config to use mongoURI
+const config = require('config');
+
+//to get any value in that json file
+const db = config.get('mongoURI');
+
+//connecting mongoDB, we need to call within server.js
+//wrap it in try - catch block  to catch any error
+const connectDB = async () => {
+    try {
+        await  mongoose.connect(db, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('MongoDb Connected...');
+    } catch(err) {
+       console.log(err.message);
+       //Exit process with failure
+       process.exit(1);
+    }
+}
+
+module.exports = connectDB;
