d79cfca526f1e8778dcaca366e3b39c60d915f61
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-11-04 00:28:43 -0500

    incorporated Add Experience functionality

--- models/Profile.js
+++ models/Profile.js
@@ -29,7 +29,7 @@ const ProfileSchema = new mongoose.Schema({
     youtubeusername: {
         type: String
     },
-    experiences: [
+    experience: [
         {
             title: {
                 type: String,
