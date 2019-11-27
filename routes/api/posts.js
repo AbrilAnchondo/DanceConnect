605a91701abab833cbf38ca4cfe33bcf09a2d4da
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-11-27 01:23:30 -0500

    added route to create a post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,14 +2,47 @@ const express = require('express');
 
 //to use express router
 const  router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
 
-//before every route you want to add 3 things:
+//bringing in all other models
+const User = require('../../models/User');
+const Profile = require('../../models/Profile');
+const Post = require('../../models/Post');
 
-//@route = request type GET api/posts <- endpoint
-//@description = Test route (other : resgister user, add profile)
-//@access = whether is public or private, do you need a token
+//@route = request type POST api/posts <- endpoint
+//@description = Add a post / create post
+//@access = whether is public or private, do you need a token? Private
+router.post('/', [ auth , [
+    check('text', 'Text is required').not().isEmpty()
+    ]
+],
+async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
 
-//this is a test route
-router.get('/', (req, res) => res.send('User posts'));
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        //token is inside req.user.id
+        //to not send password back '-password"
+    
+        const newPost = new Post ({
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        })
+
+        //when you send back the post, will get it back in the response
+        const post = await newPost.save();
+        res.json(post);
+    } catch (error) {
+        console.log(err.message)
+        res.status(500).send('Server Error')
+    }
+}
+)
 
 module.exports = router;
