903c66ee670d1d0c9d8a772df3e3942195a6f3d5
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-12-10 23:30:00 -0500

    created register form

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,13 +1,47 @@
-import React, { Component } from 'react'
+import React, { Fragment, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 
 const Register = () => {
-
-        return (
-           <div>
-               <h1>Register!</h1>
-           </div>
-        )
+    
+    return (
+        <Fragment>
+            <h1 className="">Sign Up</h1>
+            <p className=""><i className="fas fa-user"></i> Create an Account</p>
+            <form className="form">
+                <div className="form-fields">
+                    <input type="text"
+                    name="name"
+                    placeholder="Name" 
+                    required/>
+                </div>
+                <div className="form-fields">
+                    <input type="email"  
+                    name="email" 
+                    placeholder="Email" 
+                    />
+                </div>
+                <div className="form-fields">
+                    <input type="password" 
+                    name="password"
+                    placeholder="Password"
+                    minLength="6" 
+                    />
+                </div>
+                <div className="form-fields">
+                    <input type="password" 
+                    name="password2" 
+                    placeholder="Confirm Password"
+                    minLength="6" 
+                    />
+                </div>
+                <input className="btn" 
+                type="submit"
+                />
+            </form>
+            <p>You already have an account? <Link to="/login">Login</Link></p>
+        </Fragment>
+    )
     
 }
 
