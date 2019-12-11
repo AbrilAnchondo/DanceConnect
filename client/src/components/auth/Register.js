bf7fc5b46af4cf61d5f1e679712e72cc5d5f9439
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-12-11 01:02:55 -0500

    finished controlled register form, ready for fetching...

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,38 +1,70 @@
 import React, { Fragment, useState } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
 
 
 const Register = () => {
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        password: "",
+        password2: ""
+    });
+
+    //destructuring
+    const { name, email, password, password2 } = formData;
+
+    const handleChange = (e) => setFormData({ ...formData, [e.target.name]: e.target.value })
     
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        //check if passwords match
+        if (password !== password2) {
+            console.log("Passwords do not match")
+        } else {
+          console.log(formData);
+          
+        }    
+    }
+
     return (
         <Fragment>
             <h1 className="">Sign Up</h1>
             <p className=""><i className="fas fa-user"></i> Create an Account</p>
-            <form className="form">
+            <form className="form" onSubmit={(e) => handleSubmit(e)}>
                 <div className="form-fields">
                     <input type="text"
                     name="name"
-                    placeholder="Name" 
+                    placeholder="Name"
+                    value={name} 
+                    onChange={e => handleChange(e)}
                     required/>
                 </div>
                 <div className="form-fields">
                     <input type="email"  
                     name="email" 
-                    placeholder="Email" 
+                    placeholder="Email"
+                    value={email}
+                    onChange={e => handleChange(e)}
+                    required
                     />
                 </div>
                 <div className="form-fields">
                     <input type="password" 
                     name="password"
                     placeholder="Password"
-                    minLength="6" 
+                    minLength="6"
+                    value={password}
+                    onChange={e => handleChange(e)} 
                     />
                 </div>
                 <div className="form-fields">
                     <input type="password" 
                     name="password2" 
                     placeholder="Confirm Password"
-                    minLength="6" 
+                    minLength="6"
+                    value={password2}
+                    onChange={e => handleChange(e)} 
                     />
                 </div>
                 <input className="btn" 
