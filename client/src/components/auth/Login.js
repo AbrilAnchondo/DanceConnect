053de2974e0b9f864bd3175e67365f3755633983
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-12-10 22:46:56 -0500

    changed Login and Register to function components

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react'
 
-class Login extends Component {
-    state = {
-        email: "",
-        password: ""
-    }
-    render (){
+const Login = () => {
+   
         return (
             <div>
                 <h1>Login</h1>
             </div>
         )
-    }
+    
 }
 
 export default Login
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,22 +1,14 @@
 import React, { Component } from 'react'
 
 
-class Register extends Component {
+const Register = () => {
 
-    state = {
-        name: "",
-        email: "",
-        password: ""
-    }
-
-    render () {
         return (
            <div>
                <h1>Register!</h1>
            </div>
         )
-    }
-
+    
 }
 
 export default Register
